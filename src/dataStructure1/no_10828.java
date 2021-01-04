da70ecf2c505f2dbcc48b3b212d922f7a0d1b388
sk <jsk_1025@naver.com>
2021-01-04 15:45:11 +0900

    init

---
+++ src/dataStructure1/no_10828.java
@@ -0,0 +1,12 @@
+package dataStructure1;
+
+import java.util.Scanner;
+
+public class no_10828 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+    }
+}
