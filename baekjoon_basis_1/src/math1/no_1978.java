0fad896d4979118bbd2a6e19b2ea6861f10b4d33
sk <jsk_1025@naver.com>
2021-01-28 00:00:29 +0900

    소수 찾기

--- baekjoon_basis_1/src/math1/no_1978.java
+++ baekjoon_basis_1/src/math1/no_1978.java
@@ -1,4 +1,35 @@
 package math1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class no_1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            boolean flag = true;
+            int number = Integer.parseInt(st.nextToken());
+
+            if(number == 1)
+                continue;
+            for(int j = 2; j <= Math.sqrt(number); j++)
+            {
+                if(number % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == true)
+                count++;
+        }
+        System.out.println(count);
+    }
 }
