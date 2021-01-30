f3b8e492f951e368174deb9c417885abe540905f
sk <jsk_1025@naver.com>
2021-01-30 21:56:34 +0900

    팩토리얼 0의 개수

---
+++ baekjoon_basis_1/src/math1/no_1676.java
@@ -0,0 +1,21 @@
+package math1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no_1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        while(n >= 5)
+        {
+            count += n/5;
+            n = n/5;
+        }
+        System.out.print(count);
+    }
+}
