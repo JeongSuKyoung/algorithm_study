e2cd807b730c2b1851a0b1e452ac617ccaa4873d
Sukyoung <jsk_1025@naver.com>
2021-03-22 22:43:09 +0900

    시각

--- previous_problems/src/exercise/implement_2.java
+++ previous_problems/src/exercise/implement_2.java
@@ -1,2 +1,24 @@
-package exercise;public class implement_2 {
+package exercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class implement_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for(int hour = 0; hour < n+1; hour++){
+            for(int min = 0; min < 60; min++){
+                for(int sec = 0; sec < 60; sec++){
+                    if(String.valueOf(hour).contains("3") || String.valueOf(min).contains("3") || String.valueOf(sec).contains("3"))
+                        count++;
+                }
+            }
+        }
+        System.out.print(count);
+    }
 }
